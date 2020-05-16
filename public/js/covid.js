880612a2f474baf73a5fd524078e2aa2869c12b6
John O'Neil <fluffywestie@tpg.com.au>
2020-05-16 11:20:29 +1000

    Bring in DC's code

--- public/js/covid.js
+++ public/js/covid.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
+  var users = [];
 
-    var users = [];
+  //Event Handlers
+  $(document).on("click", "button.something", afunction);
 
-    //Event Handlers
-    $(document).on("click", "button.something", afunction);
+  function main() {
+    $.get("/api/user", function (data) {
+      users = data;
 
-    function main() {
+      console.log(" User Data", users);
+    });
+  }
 
-        $.get("/api/user", function (data) {
-            users = data;
+  function queryGet(get) {
+    $.ajax({
+      method: "GET",
+      url: "/api/get",
+      data: get,
+    }).then(function () {
+      console.log(get);
+    });
+  }
 
-            console.log(" User Data", users);
+  function afunction() {}
 
-        });
-    }
-
-    function afunction() {
-
-    }
-
-    // Iniital Run logic
-    main();
+  // Iniital Run logic
+  main();
 });
