b51df346bacabc2a842ba74f0cf06aa2f2b80248
DCRevResLabs <dc@revolutionresearch.com.au>
2020-05-17 22:27:39 +1000

    signup form creation

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,13 +9,14 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function (app) {
-
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // index route loads index.html
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
-
   });
 
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
 };
