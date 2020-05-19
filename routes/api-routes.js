9ae582dec028b61d23ca11712af4d263a0ed6faa
John O'Neil <fluffywestie@tpg.com.au>
2020-05-19 21:35:09 +1000

    server and client communicating

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,9 +3,50 @@ var db = require("../models");
 const axios = require('axios');
 const moment = require('moment');
 
+// Functions 
+// Call Covid API taking user Input
+function callAPI(query_region) {
+
+  // Set up localvars
+  var returnData = {};
+  const todayDate = new Date();
+
+  var year = '' + todayDate.getFullYear();
+  var month = '' + (todayDate.getMonth() + 1);
+  var day = '' + (todayDate.getDate() - 1);
+
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
+
+  let date = `${year}-${month}-${day}`;
+
+  console.log(`Using Date of ${date}`);
+
+  var queryPart1 = "https://covid-api.com/api/reports?";
+  var queryPart2 = "date=" + date;
+  var queryPart3 = "&q=" + query_region;
+
+  var queryURL = queryPart1 + queryPart2 + queryPart3;
+
+  console.log(queryURL);
+
+  var totalDeaths = 0;
+  var totalRecovered = 0;
+
+  axios.get(queryURL)
+    .then(response => {
+      console.log(response.data);
+      response.data.data.forEach(element => {
+        totalDeaths += element.deaths;
+        totalRecovered += element.recovered;
+      });
+      console.log(`As of ${todayDate.toLocaleDateString()} there are Deaths:  ${totalDeaths}  Recovered: ${totalRecovered}`);
+    })
+}
 
 // Routes
-// =============================================================
 module.exports = function (app) {
 
   // GET route XXXXXXXX
@@ -20,63 +61,12 @@ module.exports = function (app) {
   });
 
   // GET route data for a week
-  app.get("/api/getdata", function (req, res) {
+  app.post("/api/getdata", function (req, res) {
+    console.log('####' + req.body)
 
-    var responseData = {};
-
-    // User queries
-    var userChoices = req.query.desired_attributes;
-
-    // Call callAPI for 7 days of data
-    responseData = callAPI(req.query.query_region, req.query.desired_attributes)
-
-    res.json(responseData).end();
+    res.json({
+      data: true // todo: replace with the data that we need for the ui
+    })
   });
 
-
-  // Call Covid API taking user Input
-  function callAPI(query_region, desired_attributes) {
-
-    // Set up localvars
-    var returnData = {};
-    const todayDate = new Date();
-
-    var year = '' + todayDate.getFullYear();
-    var month = '' + (todayDate.getMonth() + 1);
-    var day = '' + (todayDate.getDate() - 1);
-
-    if (month.length < 2)
-      month = '0' + month;
-    if (day.length < 2)
-      day = '0' + day;
-
-    let date = `${year}-${month}-${day}`;
-
-    console.log(`Using Date of ${date}`);
-
-    var queryPart1 = "https://covid-api.com/api/reports?";
-    var queryPart2 = "date=" + date;
-    var queryPart3 = "&q=Australia";
-
-    var queryURL = queryPart1 + queryPart2 + queryPart3;
-
-    console.log(queryURL);
-
-    var totalDeaths = 0;
-    var totalRecovered = 0;
-
-    axios.get(queryURL)
-      .then(response => {
-        console.log(response.data);
-        response.data.data.forEach(element => {
-          totalDeaths += element.deaths;
-          totalRecovered += element.recovered;
-        });
-        console.log(`As of ${todayDate.toLocaleDateString()} there are Deaths:  ${totalDeaths}  Recovered: ${totalRecovered}`);
-
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
 };
